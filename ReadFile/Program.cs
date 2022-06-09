7d20219f9812d4757877e0e71d73844604cacd91
CamDavi5 <camrdavis.biz@gmail.com>
2022-06-09 17:16:15 -0500

    Add project files.

---
+++ ReadFile/Program.cs
@@ -0,0 +1,7 @@
+﻿
+string[] readText = System.IO.File.ReadAllLines(/*"CoolStoryBro.txt pathname*/);
+System.Console.WriteLine("Contents of CoolStoryBro.txt = ");
+foreach (string text in readText)
+{
+    Console.WriteLine("\t" + text);
+}
